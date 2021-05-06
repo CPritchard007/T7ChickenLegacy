807ab47d20382ad71be3a3b266f2270a46e6028e
Curtis Pritchard <curtis.pritchard@stclaircconnect.ca>
2021-05-06 16:44:34 -0400

    created a much better styled status menu

--- src/DataPages/Moves.js
+++ src/DataPages/Moves.js
@@ -1,31 +1,38 @@
 import React from 'react';
 import { ScrollView, View, StyleSheet, Text } from 'react-native';
-import { BACKGROUND_COLOR, globalTheme } from '../../assets/themes/defaultTheme';
+import { BACKGROUND_COLOR, globalTheme, SECONDARY_COLOR } from '../../assets/themes/defaultTheme';
 
-
+const border_width = 2
 
 export const Move = ({navigator}) => {
     const style = StyleSheet.create({
-        tableCellHeight: {
-            height: 40,
-            borderColor: 'grey',            
-        },
+        
         headerContainerStyle: {
             justifyContent: 'center',
             flex: 1,
-            backgroundColor: '#bdbdbd',
-            height: 40,
-            borderColor: 'grey',
-            borderTopWidth: 3, borderLeftWidth: 3, borderBottomWidth: 3
+            backgroundColor: '#232323',
+            height: 44,
+            borderColor: '#121212',
+            borderLeftWidth: border_width,
+            borderTopWidth: border_width,
+            borderBottomWidth: border_width,
+            borderTopWidth: border_width,
+            borderBottomColor: SECONDARY_COLOR
         },
         headerTextStyle: {
+            height: 40,
+            width: 96 /* Change This */,
+            backgroundColor: '#232323',
+            flex: 1, justifyContent: 'center',
+            borderLeftWidth: border_width,
+            borderColor: '#121212'
         }
     });
     const TableHeader = () => {
         return (
-            <View style={style.tableCellHeight}>
+            <View>
                 <View style={globalTheme.horizontalWrap}>
-                    <View style={style.headerContainerStyle}><Text style={{textAlign: 'center'}}>Notation</Text></View>
+                    <View style={style.headerContainerStyle}><Text style={{textAlign: 'center', color:'#fff'}}>Notation</Text></View>
                 </View>
             </View>
         ) 
@@ -36,23 +43,24 @@ export const Move = ({navigator}) => {
         );
     } 
     return (
-        <ScrollView style={globalTheme.mainContainer} >
+        <ScrollView style={globalTheme.mainContainer} bounces={false} overScrollMode={'never'} showsHorizontalScrollIndicator={false}>
             <View style={{flexDirection: 'row'}}>
-                <View  style={{width: '50%'}}>
+                <View  style={{width: '50%'}}> 
                     <TableHeader />
-                    
 
                 </View>
-                <ScrollView horizontal={true} style={{flex: 1, borderColor: 'grey'}} bounces={false}  overScrollMode={'never'} snapToInterval={96} showsHorizontalScrollIndicator={false}>
-                    <View style={{flexDirection: 'row', borderRightWidth: 3, borderColor: 'grey'}}>
-                        <View style={{height: 40, width: 96 /* Change This */, backgroundColor: '#bdbdbd', flex: 1, justifyContent: 'center', borderTopWidth: 3, borderLeftWidth: 3, borderBottomWidth: 3, borderColor: 'grey'}}><Text style={{textAlign: 'center'}}>Speed</Text></View>
-                        <View style={{height: 40, width: 96 /* Change This */, backgroundColor: '#bdbdbd', flex: 1, justifyContent: 'center', borderTopWidth: 3, borderLeftWidth: 3, borderBottomWidth: 3, borderColor: 'grey'}}><Text style={{textAlign: 'center'}}>On Block</Text></View>
-                        <View style={{height: 40, width: 96 /* Change This */, backgroundColor: '#bdbdbd', flex: 1, justifyContent: 'center', borderTopWidth: 3, borderLeftWidth: 3, borderBottomWidth: 3, borderColor: 'grey'}}><Text style={{textAlign: 'center'}}>On Hit</Text></View>
-                        <View style={{height: 40, width: 96 /* Change This */, backgroundColor: '#bdbdbd', flex: 1, justifyContent: 'center', borderTopWidth: 3, borderLeftWidth: 3, borderBottomWidth: 3, borderColor: 'grey'}}><Text style={{textAlign: 'center'}}>Counter Hit</Text></View>
-                        <View style={{height: 40, width: 96 /* Change This */, backgroundColor: '#bdbdbd', flex: 1, justifyContent: 'center', borderTopWidth: 3, borderLeftWidth: 3, borderBottomWidth: 3, borderColor: 'grey'}}><Text style={{textAlign: 'center'}}>Hit Level(s)</Text></View>
-                        <View style={{height: 40, width: 96 /* Change This */, backgroundColor: '#bdbdbd', flex: 1, justifyContent: 'center', borderTopWidth: 3, borderLeftWidth: 3, borderBottomWidth: 3, borderColor: 'grey'}}><Text style={{textAlign: 'center'}}>Damage</Text></View>
+                <ScrollView horizontal={true} bounces={false} overScrollMode={'never'} snapToInterval={96} showsHorizontalScrollIndicator={false}>
+                    <View style={{height: 300, }}>
+                        <View style={{flexDirection: 'row', borderRightWidth: border_width, borderTopWidth: border_width, borderBottomWidth: border_width, borderColor: '#121212', borderBottomColor: SECONDARY_COLOR}}>
+                            <View style={style.headerTextStyle}><Text style={{textAlign: 'center', color:'#fff'}}>Speed</Text></View>
+                            <View style={style.headerTextStyle}><Text style={{textAlign: 'center', color:'#fff'}}>On Block</Text></View>
+                            <View style={style.headerTextStyle}><Text style={{textAlign: 'center', color:'#fff'}}>On Hit</Text></View>
+                            <View style={style.headerTextStyle}><Text style={{textAlign: 'center', color:'#fff'}}>Counter Hit</Text></View>
+                            <View style={style.headerTextStyle}><Text style={{textAlign: 'center', color:'#fff'}}>Hit Levels</Text></View>
+                            <View style={style.headerTextStyle}><Text style={{textAlign: 'center', color:'#fff'}}>Damage</Text></View>
+                        </View>
+                        
                     </View>
-
                 </ScrollView>
             </View>
         </ScrollView>
